c8653f5f80c5a9d04a6bffbfe7ea74a4ab5cae2c
Sarang-Bangade <bangadesarang@gmail.com>
2025-12-01 16:25:29 +0530

    commit

--- bio_generator 2.py
+++ bio_generator 2.py
@@ -38,4 +38,4 @@ if save =='y':
     filename = f"{name.lower().replace('','_')}_bio.txt"
     with open(filename, "w", encoding="utf-8") as f:
         f.write(bio)
-    print("file saved")    
+    print("file saved")  
